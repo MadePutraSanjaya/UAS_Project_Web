c4524db0f830a6c5cad811769de2eff8e1ee98c4
I Made Adi Putra Sanjaya <madeputra.6166@gmail.com>
2024-07-05 18:55:35 +0800

    admin

--- admin/db/uas.sql
+++ admin/db/uas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Waktu pembuatan: 05 Jul 2024 pada 07.39
+-- Waktu pembuatan: 05 Jul 2024 pada 10.55
 -- Versi server: 5.7.39
 -- Versi PHP: 8.2.0
 
@@ -34,6 +34,15 @@ CREATE TABLE `categories` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data untuk tabel `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Pantai', '2024-07-05 10:55:01', '2024-07-05 10:55:01'),
+(2, 'Gunung', '2024-07-05 10:55:01', '2024-07-05 10:55:01'),
+(3, 'Kota', '2024-07-05 10:55:01', '2024-07-05 10:55:01');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +58,15 @@ CREATE TABLE `discounts` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `discounts`
+--
+
+INSERT INTO `discounts` (`id`, `name`, `description`, `expired_at`, `percentage`, `created_at`, `updated_at`) VALUES
+(1, 'Diskon Awal Tahun', 'Diskon untuk awal tahun', '2025-01-31', '10.00', '2024-07-05 10:55:01', '2024-07-05 10:55:01'),
+(2, 'Promo Musim Panas', 'Diskon besar untuk liburan musim panas', '2024-08-31', '20.00', '2024-07-05 10:55:01', '2024-07-05 10:55:01'),
+(3, 'Promo Akhir Tahun', 'Diskon spesial menyambut tahun baru', '2024-12-31', '15.00', '2024-07-05 10:55:01', '2024-07-05 10:55:01');
 
 -- --------------------------------------------------------
 
@@ -68,6 +86,15 @@ CREATE TABLE `travels` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `travels`
+--
+
+INSERT INTO `travels` (`id`, `name`, `description`, `price`, `day_count`, `night_count`, `discount_id`, `category_id`, `created_at`, `updated_at`) VALUES
+(1, 'Liburan Pantai Bali', 'Nikmati liburan di pantai Bali yang indah', '200.00', 5, 4, 1, 1, '2024-07-05 10:55:01', '2024-07-05 10:55:01'),
+(2, 'Pendakian Gunung Rinjani', 'Petualangan mendaki gunung Rinjani yang menantang', '300.00', 3, 2, 2, 2, '2024-07-05 10:55:01', '2024-07-05 10:55:01'),
+(3, 'Jelajah Kota Malang', 'Jelajahi keindahan dan sejarah kota Malang', '150.00', 4, 3, 3, 3, '2024-07-05 10:55:01', '2024-07-05 10:55:01');
 
 --
 -- Indexes for dumped tables
@@ -101,19 +128,19 @@ ALTER TABLE `travels`
 -- AUTO_INCREMENT untuk tabel `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `discounts`
 --
 ALTER TABLE `discounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `travels`
 --
 ALTER TABLE `travels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
