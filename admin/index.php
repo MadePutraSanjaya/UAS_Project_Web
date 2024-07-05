5a2cd987da15ce6723f547247ba200afa30bc4df
I Made Adi Putra Sanjaya <madeputra.6166@gmail.com>
2024-07-05 18:17:24 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,19 +1,3 @@
-<?php
-$host = 'localhost:8000';
-$dbname = 'uas'; 
-$username = 'root';
-$password = 'root'; 
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    var_dump($pdo);
-die();
-
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
